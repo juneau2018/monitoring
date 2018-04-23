5705a069a873ccffe5b9d7007c44be1eda3c0e8f
詹叶 <zhany@HeisenbergMAC.local>
2018-04-24 00:59:33 +0800

    add

---
+++ src/test/java/com/piggymetrics/monitoring/MonitoringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.piggymetrics.monitoring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = MonitoringApplication.class)
+public class MonitoringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
